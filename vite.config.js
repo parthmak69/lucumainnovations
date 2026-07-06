b935f3a9a5f7274ed15aecf4a611505f84f9289b
parthmak69 <218966357+Sid-Dan06@users.noreply.github.com>
2026-07-06 17:52:37 +0530

    fix: utilize native 200.html client fallback routing for modern vite compilation

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    vercel()
+    babel({ presets: [reactCompilerPreset()] })
   ]
 })
